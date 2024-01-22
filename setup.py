abf6d9b1844368b6a56255cad2d57be365a2eecb
Christopher Oak <33976926+chroakPRO@users.noreply.github.com>
2024-01-22 20:00:32 +0100

    version 0.1.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='aisort',
-    version='0.1.1',  # Update this with every release
+    version='0.1.2',  # Update this with every release
     packages=find_packages(),
     include_package_data=True,
     package_data={
